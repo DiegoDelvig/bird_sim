46ab7c3ee150d72c80b832deb69bc6c11fc15753
DiegoDelvig <diego.delvig@gmail.com>
2026-06-02 16:24:44 +0200

    Ajout de la GUI

--- main.c
+++ main.c
@@ -2,6 +2,9 @@
 #include <stdlib.h>
 #include <math.h>
 
+#define RAYGUI_IMPLEMENTATION
+#include "raygui.h"
+
 #define MAX_BIRDS 100
 #define SCREEN_WIDTH 800
 #define SCREEN_HEIGHT 600
@@ -29,18 +32,19 @@ int main(void) {
         initBird(&flock[i]);
     }
 
+    // Variables changeable en temps réelle
+    float separation_radius = 40.0;
+    float cohesion_radius = 120.0;
+    float separation_force = 0.5;
+
+
     while (!WindowShouldClose()) {
         for (int i = 0; i < MAX_BIRDS; i++) {
 
-            // Cohésion
             Vector2 center_of_mass = {0, 0};
             int birds_in_sight = 0;
-            float cohesion_radius = 120.0;
-
-            // Séparation
             Vector2 separation = {0, 0};
             int birds_too_close = 0;
-            float separation_radius = 40.0;
 
             for (int j = 0; j < MAX_BIRDS; j++) {
                 if (i != j) {
@@ -71,8 +75,8 @@ int main(void) {
                 separation.x /= birds_too_close;
                 separation.y /= birds_too_close;
 
-                flock[i].vel.x += separation.x * 0.5;
-                flock[i].vel.y += separation.y * 0.5;
+                flock[i].vel.x += separation.x * separation_force;
+                flock[i].vel.y += separation.y * separation_force;
             }
 
             // Attraction
@@ -98,22 +102,46 @@ int main(void) {
             flock[i].pos.x += flock[i].vel.x;
             flock[i].pos.y += flock[i].vel.y;
 
-            // Collisions
-            if (flock[i].pos.x < 0 || flock[i].pos.x > SCREEN_WIDTH){
-                flock[i].vel.x *= -1;
+            // WALLS
+            if (flock[i].pos.x > SCREEN_WIDTH) {
+                flock[i].pos.x -= SCREEN_WIDTH;
             }
-            if (flock[i].pos.y < 0 || flock[i].pos.y > SCREEN_HEIGHT){
-                flock[i].vel.y *= -1;
+            if (flock[i].pos.y > SCREEN_HEIGHT) {
+                flock[i].pos.y -= SCREEN_HEIGHT;
             }
+            if (flock[i].pos.x < 0) {
+                flock[i].pos.x += SCREEN_WIDTH;
+            }
+            if (flock[i].pos.y < 0) {
+                flock[i].pos.y += SCREEN_HEIGHT;
+            }
+
         }
 
         BeginDrawing();
         ClearBackground(RAYWHITE);
 
+        // Dessin oiseau
         for (int i = 0; i < MAX_BIRDS; i++) {
             float angle = atan2f(flock[i].vel.y, flock[i].vel.x) * RAD2DEG + 90;
             DrawPoly(flock[i].pos, 3, 12, angle, SKYBLUE);
         }
+
+        // Dessin GUI
+        DrawRectangle(10, 10, 250, 150, Fade(LIGHTGRAY, 0.8));
+        DrawText("Paramètres", 20, 20, 10, DARKGRAY);
+
+        GuiSliderBar((Rectangle){110, 50, 100, 15}, "Rayon de séparation",
+                     TextFormat("0.0", separation_radius), &separation_radius,
+                     10.0, 100.0);
+        GuiSliderBar((Rectangle){110, 80, 100, 15}, "Rayon de cohésion",
+                     TextFormat("0.0", cohesion_radius), &cohesion_radius,
+                     50.0, 300.0);
+        GuiSliderBar((Rectangle){110, 110, 100, 15}, "Force esquive",
+                     TextFormat("0.2", separation_force), &separation_force,
+                     0.1, 2.0);
+
+
         EndDrawing();
     }
     CloseWindow();
